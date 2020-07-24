d4f7cbb8ef25240bf04ed7cdb6a6e378ce95b1fc
Tony Rosler <tony.rosler246@gmail.com>
2020-07-24 18:48:50 +0200

    thousend snakes update

--- thousendsnakes/0_______.js
+++ thousendsnakes/0_______.js
@@ -1,22 +1,19 @@
-function setup(){
-	createCanvas(400, 400);
+function setup() {
+  createCanvas(500, 500);
 }
-
 
 // let color = [random(255), random(100,200), random(100)]
 let r = 255;
 let g = 255;
 let b = 255;
-    
+
 function draw() {
-  
   fill(g, r, b);
   ellipse(mouseX, mouseY, 80, 80);
 }
-
 
 setInterval(() => {
   r = random(255);
   g = random(100, 200);
   b = random(100);
-  }, 5000)
+}, 5000);
